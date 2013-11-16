18b7ca8b92cc20848a6b7f99ca41d11756ef88ee
Felix Geisendörfer <felix@debuggable.com>
2013-11-16 19:33:13 +0100

    fix example running

--- test.sh
+++ test.sh
@@ -6,11 +6,11 @@ set -eu
 echo "----> Running tests"
 go test -v .
 pushd examples > /dev/null
-for example in "$(find . -type f -name '*.go')"; do
+for example in $(find . -type f -name '*.go'); do
   echo "----> Running example: ${example}"
-  go build "${example}"
-  example_bin="${example//.go/}"
-  "${example_bin}" || true
-  rm "${example_bin}"
+  bin="example-$(basename $(dirname "${example}"))"
+  go build -o "${bin}" "${example}"
+  "./${bin}" || true
+  rm -rf "${bin}"
 done
 popd > /dev/null
