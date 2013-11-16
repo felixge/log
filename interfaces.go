43b2586db5dd7432019f76e06a6a34d6aeb04daa
Felix Geisendörfer <felix@debuggable.com>
2013-11-16 11:57:59 +0100

    Move interfaces

--- interfaces.go
+++ interfaces.go
@@ -1,6 +1,26 @@
 package log
 
+// Interface defines the log interface provided by this package. Use this when
+// passing *Logger instances around.
+type Interface interface {
+	Debug(args ...interface{})
+	Info(args ...interface{})
+	Warn(args ...interface{})
+	Error(args ...interface{}) error
+	Fatal(args ...interface{})
+	Panic()
+}
+
+// Handler is used to implement log handlers.
+type Handler interface {
+	// HandleLog processes the given Entry (e.g. writes it to a file, sends it to
+	// a log service)
+	HandleLog(Entry)
+	// Flush waits for any buffered data to be flushed and blocks new calls
+	// to HandleLog until it returns.
+	Flush()
+}
+
 type Formatter interface {
 	Format(e Entry) string
 }
-
--- logger.go
+++ logger.go
@@ -51,27 +51,6 @@ var levels = map[Level]string{
 	FATAL: "fatal",
 }
 
-// Interface defines the log interface provided by this package. Use this when
-// passing *Logger instances around.
-type Interface interface {
-	Debug(args ...interface{})
-	Info(args ...interface{})
-	Warn(args ...interface{})
-	Error(args ...interface{}) error
-	Fatal(args ...interface{})
-	Panic()
-}
-
-// Handler is used to implement log handlers.
-type Handler interface {
-	// HandleLog processes the given Entry (e.g. writes it to a file, sends it to
-	// a log service)
-	HandleLog(Entry)
-	// Flush waits for any buffered data to be flushed and blocks new calls
-	// to HandleLog until it returns.
-	Flush()
-}
-
 type Config struct {
 	FlushTimeout time.Duration
 	FatalExit    bool
